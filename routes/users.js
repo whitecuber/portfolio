6028ef4252be95188374c70093750efda69294f8
do7be <bomsap700@gmail.com>
2017-02-28 22:23:30 +0900

    add mongo

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var ObjectID = require('mongodb').ObjectID;
+var collection = require('../lib/mongo');
+var COL = 'portfolio';
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    collection(COL).find().toArray(function(err, docs) {
+        res.send(docs);
+    })
+});
+
+// POST insert data
+router.post('/', function(req, res) {
+    collection(COL).insertOne(req.body).then(function(r) {
+        res.send(r);
+    });
 });
 
 module.exports = router;
