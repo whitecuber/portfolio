a5c76cdd0c1e9ec76dacd56678ef7166086f8dce
do7be <bomsap700@gmail.com>
2017-03-14 21:23:31 +0900

    fix const name

--- routes/page.js
+++ routes/page.js
@@ -5,10 +5,8 @@ var fs = require('fs')
 var aws = require('aws-sdk')
 
 const S3_ACCESS_KEY_ID = process.env.S3_ACCESS_KEY_ID
-const S3_SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY
+const S3_SECRET_ACCESS_KEY = process.env.S3_SECRET_ACCESS_KEY
 const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME
-
-console.log(S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY, S3_BUCKET_NAME)
 
 aws.config.update({
     accessKeyId: S3_ACCESS_KEY_ID,
